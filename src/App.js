00a2b7e521fbf5980e0f86e92993538e428947a3
hirakida <dantuba19990326@gmail.com>
2024-11-23 22:30:12 +0900

    Add: 簡易的なContactコンポーネントの作成

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Header } from './components/Header';
 import { Skills } from './components/Skills';
+import { Contact } from './components/Contact';
 
 function App() {
   return (
     <>
       <Header />
       <Skills />
+      <Contact />
     </>
   );
 }
