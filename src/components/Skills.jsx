62faf521f19241416c4df2293504a3a0cdf174af
hirakida <dantuba19990326@gmail.com>
2024-11-27 22:49:14 +0900

    mod: useReducerを用いたステートの更新

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,21 +1,24 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer } from 'react';
 import axios from 'axios';
+import { skillReducer, initialState, actionTypes } from '../reducers/skillReducer';
 
 export const Skills = () => {
-  const [languageList, setLanguageList] = useState([]);
-  console.log(languageList);
+  const [state, dispatch] = useReducer(skillReducer, initialState);
 
   // 空配列のため、マウント(初回レンダリング時)のみ実行
   // useEffectの第1引数は関数自体を渡す
   // axiosはコールバックで記載することで、処理の成功失敗によって処理を変えることができる
   // responseを引数として受け取れるのは、axiosとPromiseの仕組み。コールバック関数が引数を受け取れるようなものではない。
   useEffect(() => {
-    axios.get('https://api.github.com/users/tsubasahirakida/repos')
+    dispatch({ type: actionTypes.fetch });
+    axios.get('https://api.github.com/users/USER_NAME/repos')
       .then((response) => {
-        // map関数にコールバック関数を渡して、配列作成
         const languageList = response.data.map(res => res.language);
         const countedLanguageList = generateLanguageCountObj(languageList);
-        setLanguageList(countedLanguageList);
+        dispatch({ type: actionTypes.success, payload: { languageList: countedLanguageList } });
+      })
+      .catch(() => {
+        dispatch({ type: actionTypes.error });
       });
   }, []);
 
